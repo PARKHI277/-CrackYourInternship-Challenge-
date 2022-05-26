b746284005a0e1393670c7f84f755c0c9892b2ee
Parkhi <parkhigarg277@gmail.com>
2022-05-26 18:29:11 +0530

    happynumber

---
+++ Maths/happy.cpp
@@ -0,0 +1,12 @@
+ bool isHappy(int n) {
+        if(n==1||n==7)
+            return true;
+        if(n<10)
+            return false;
+        int temp=0;
+        while(n){
+            temp+=(n%10)*(n%10);
+            n/=10;
+        }
+        return isHappy(temp);
+    }
